d6609f2541081344e1acbf330ca908b44a236ac7
Ota Fejfar <ota.fejfar@gmail.com>
2026-08-26 12:14:40 +0200

    feat: Ble_manager - Choosing to either advertise or attempt to directly connect to a bonded peer
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

--- Ble_manager/Ble_manager.cpp
+++ Ble_manager/Ble_manager.cpp
@@ -610,13 +610,31 @@ inline void BleManager::state_enable_process( void )
     /* Enable the ble composite device */
     enable_config.current_channel_id = p_channel_current->id;
 
+#warning Now that bonding is successful with the bond code, we should provide the blecdev with the chosen peer to connect to
+
     result = blecdev_enable( &enable_config );
     ASSERT_DYGMA( result == RESULT_OK, "blecdev_enable failed" );
     EXIT_IF_ERR( result, "blecdev_enable failed" );
 
-    result = blecdev_adv_start_whitelist();
-    ASSERT_DYGMA( result == RESULT_OK, "blecdev_adv_start_whitelist failed" );
-    EXIT_IF_ERR( result, "blecdev_adv_start_whitelist failed" );
+#warning Now that bonding is successful with the bond code, we should continue here with the bonded device search
+
+    /* Check if the current channel peer ID is not bonded. */
+    if( p_channel_current->peer_id == PM_PEER_ID_INVALID )
+    {
+        /* The peer is not bonded on this channel, hence we start general advertising over all possible connections. */
+        result = blecdev_adv_start();
+        ASSERT_DYGMA( result == RESULT_OK, "blecdev_adv_start failed" );
+        EXIT_IF_ERR( result, "blecdev_adv_start failed" );
+    }
+    else
+    {
+        /* The peer is bonded on this channel, hence we start advertising over whitelisted connections. */
+
+#warning "This will whitelist all bonded devices. We might need to filter just one device with this ID"
+        result = blecdev_peer_conn_start( p_channel_current->peer_id );
+        ASSERT_DYGMA( result == RESULT_OK, "blecdev_adv_start_whitelist failed" );
+        EXIT_IF_ERR( result, "blecdev_adv_start_whitelist failed" );
+    }
 
 
     enabled_flag = true;
